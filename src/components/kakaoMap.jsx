fac26dbb87fb63e2776861a0a59d035078a6df15
leekjoo <leekjoo1008@gmail.com>
2024-12-26 16:38:28 +0900

    feat: search

--- src/components/kakaoMap.jsx
+++ src/components/kakaoMap.jsx
@@ -2,12 +2,37 @@ import React from "react";
 import { Map } from "react-kakao-maps-sdk";
 
 function KakaoMap(props) {
+  const searchPlaces = (e) => {
+    e.preventDefault();
+    const keyword = e.target.keyword.value;
+    const ps = new kakao.maps.services.Places();
+    ps.keywordSearch(keyword, placesSearchCB);
+  };
+
+  function placesSearchCB(data, status) {
+    if (status === kakao.maps.services.Status.OK) {
+      console.log("data", data);
+    } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+      alert("검색 결과가 존재하지 않습니다.");
+      return;
+    } else if (status === kakao.maps.services.Status.ERROR) {
+      alert("검색 결과 중 오류가 발생했습니다.");
+      return;
+    }
+  }
   return (
-    <Map
-      center={{ lat: 33.450701, lng: 126.570667 }}
-      style={{ width: "1000px", height: "600px" }}
-      level={3}
-    />
+    <div>
+      <form onSubmit={searchPlaces}>
+        <input type="text" name="keyword" />
+        <button type="submit">검색하기</button>
+      </form>
+
+      <Map
+        center={{ lat: 33.450701, lng: 126.570667 }}
+        style={{ width: "1000px", height: "600px" }}
+        level={3}
+      />
+    </div>
   );
 }
 
